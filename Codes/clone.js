25a6803e6fa2e4fafb39cf6de3226417e3e97fbc
Vishal Kumar <kumarvishalmast@gmail.com>
2025-07-07 14:17:43 +0530

    Update clone.js

--- Codes/clone.js
+++ Codes/clone.js
@@ -1,46 +1,63 @@
-function clone(obj, visited = new WeakMap()){
-    if (typeof obj !== "object" || obj === null) return obj;
-    
+function deepClone(obj, visited = new WeakMap()) {
+    // Handle primitives and null
+    if (obj === null || typeof obj !== "object") return obj;
+
+    // Handle cycles
     if (visited.has(obj)) return visited.get(obj);
-    if (Array.isArray(obj)) {
-        let res = Array(obj.length);
-        visited.set(obj, res);
-        for(let i = 0; i < obj.length; i++){
-            if (!(i in obj)) continue;
-            res[i] = clone(obj[i], visited);
-        }
-        return res;
-    }
+
+    // WeakMap and WeakSet (can't clone contents)
+    if (obj instanceof WeakMap) return new WeakMap();
+    if (obj instanceof WeakSet) return new WeakSet();
+
+    // Date
     if (obj instanceof Date) return new Date(obj);
-    
-    if (obj instanceof Function) return obj;
-    
-    if (obj instanceof Map){
-        let res = new Map();
+
+    // RegExp
+    if (obj instanceof RegExp) return new RegExp(obj);
+
+    // Map
+    if (obj instanceof Map) {
+        const res = new Map();
         visited.set(obj, res);
-        for (let [key, value] of obj){
-            res.set(key, clone(value, visited));
+        for (const [key, value] of obj.entries()) {
+            res.set(deepClone(key, visited), deepClone(value, visited));
         }
         return res;
     }
-    
-    if (obj instanceof Set){
-        let res = new Set();
+
+    // Set
+    if (obj instanceof Set) {
+        const res = new Set();
         visited.set(obj, res);
-        for(let value of obj){
-            res.add(clone(value, visited));
+        for (const value of obj.values()) {
+            res.add(deepClone(value, visited));
         }
         return res;
     }
-    
-    let res = {};
+
+    // Array
+    if (Array.isArray(obj)) {
+        const res = [];
+        visited.set(obj, res);
+        for (let i = 0; i < obj.length; i++) {
+            res[i] = deepClone(obj[i], visited);
+        }
+        return res;
+    }
+
+    // Function (cannot deep clone function body/closure)
+    if (typeof obj === "function") return obj;
+
+    // Symbol (returns a new symbol with same description)
+    if (typeof obj === "symbol") return Symbol(obj.description);
+
+    // Generic object (preserve prototype)
+    const res = Object.create(Object.getPrototypeOf(obj));
     visited.set(obj, res);
-    for (const key of Object.keys(obj)){
-        res[key] = clone(obj[key], visited);
+
+    for (const key of Reflect.ownKeys(obj)) {
+        res[key] = deepClone(obj[key], visited);
     }
-    
-    for (const sym of Object.getOwnPropertySymbols(obj)){
-        res[sym] = clone(obj[sym], visited);
-    }
+
     return res;
 }
